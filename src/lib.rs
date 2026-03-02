f7439c55eb2f362eadf588eed41212f8aeb7562d
jabberwocky238 <7176656@qq.com>
2026-03-01 20:38:58 -0500

    fs3: 分层

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ pub mod types;
 pub mod components;
 
 pub use types::s3::{S3Binding};
-pub use components::s3_handler::router as axum_router;
+pub use components::s3_axum_handler::router as axum_router;
 
